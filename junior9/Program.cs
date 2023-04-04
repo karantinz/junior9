1278b22b34839eeea72f6e0559e56c4a3c248de5
Zmeegolov <karantinz@mail.ru>
2023-04-04 21:06:49 +0300

    строки

--- junior9/Program.cs
+++ junior9/Program.cs
@@ -2,10 +2,12 @@
 string exitProgram = "exit";
 
 Console.WriteLine($"Если хотите выйти из программы, введите {exitProgram}");
+
 while (input != exitProgram)
 {
     Console.Write("Введите слово: ");
     input = Console.ReadLine();
     Console.WriteLine($"Ваше слово: {input}");
 }
+
 Console.WriteLine("Вы вышли из программы");
